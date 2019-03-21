dadd9ada2f19b1e27300367728d2beb9a0b9e06d
Oungoun <30619528+Oungoun@users.noreply.github.com>
2019-03-21 13:17:56 +0100

    Gauthier

--- For_Gauthier/Column_issue.py
+++ For_Gauthier/Column_issue.py
@@ -2,33 +2,33 @@ import pandas as pd
 import numpy as np
 
 # import données
-path="C:/Users/benjamin.schick/Desktop/GitHub/Tools/For_Gauthier/document activités.xlsx"
-sheetName="Feuil1"
+path="C:/Users/benjamin.schick/Desktop/GitHub/Tools/For_Gauthier/Meeting Michel Site Faurecia.xlsx"
+sheetName="Detail Site per Site"
 data = pd.read_excel(path, sheetName)
 data.index = range(0, len(data))
 print(data.head())
 
 data=data[2:len(data)]
 #retirer les yes, no, none du tableau
-list_to_delete=['Yes','No','No activities related to general metal working','No activities related to Machinery / Apparatus production','No activities related to Wire / Cable production production','No activities related to Petrochemicals Base Products','No activities related to Chemical Commodities','No other Miscellaneaous Chemical Operations and Commodities','No activities related to Plastics','No activities related to Rubber', 'No activities related to Processing Fibers','No activities related to (Pre-) Spinning','No activities related to Weaving / Twisting','No textile finishing activities','No activities related to textile processing/products','No basic industry activity','No activities related to Building Materials, Ceramics and Glass”,“No Leather/Wood activities']
-for i in range (1, len(data)-1):
+list_to_delete = ['Yes','No','NONE','None','none' ,'No activities related to general metal working','No activities related to Machinery / Apparatus production','No activities related to Wire / Cable production production','No activities related to Petrochemicals Base Products','No activities related to Chemical Commodities','No other Miscellaneaous Chemical Operations and Commodities','No activities related to Plastics','No activities related to Rubber', 'No activities related to Processing Fibers','No activities related to (Pre-) Spinning','No activities related to Weaving / Twisting','No textile finishing activities','No activities related to textile processing/products','No basic industry activity','No activities related to Building Materials, Ceramics and Glass”,“No Leather/Wood activities']
+for i in range(1, len(data)-1):
     for j in range(1,len(data.iloc[1,])-1):
-        if list_to_delete.count(data.iloc[i,j])==1:
-            data.iloc[i,j]=float('nan')
+        if list_to_delete.count(data.iloc[i, j]) == 1:
+            data.iloc[i, j] = float('nan')
 arr=[]
-for i in range (4, len(data)-1):
-    temp=str(data.iloc[i,0])
-    for j in range(1,len(data.iloc[1,])-1):
-        val=data.iloc[i,j]
+for i in range(4, len(data)-1):
+    temp = str(data.iloc[i, 0])
+    for j in range(1, len(data.iloc[1, ])-1):
+        val=data.iloc[i, j]
         if not pd.isnull(val):
             print(val)
-            temp=temp+'%%'+str(val)
+            temp = temp+'%%'+str(val)
     arr.append(temp)
 
-df=pd.DataFrame(arr)
-df=np.split(df, '@', axis=1)
+df = pd.DataFrame(arr)
+#df = np.split(df, '%%', axis=1)
 #save données
-name='interdependance2'
-writer = pd.ExcelWriter(str(name) + ".xlsx")
+name = str('interdependance_20190321_2')
+writer = pd.ExcelWriter(name + ".xlsx")
 df.to_excel(writer, 'Sheet1')
 writer.save()
